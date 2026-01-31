1fb74c4a2ce768c8c87ecf9c3e9a7eca4353131d
schirmr <schirmr@outlook.com>
2026-01-31 20:07:29 -0300

    catch mem info

---
+++ README.md
@@ -0,0 +1,2 @@
+/proc/meminfo - contem informações do uso de memória ram.
+/proc/uptime - contem o tempo em segundos que o sistema está ligado.
--- main.c
+++ main.c
@@ -3,9 +3,10 @@
 #include <string.h>
 #include <unistd.h>
 
-int ler_arquivo(const char *nome_arquivo, const char *info){
+long ler_arquivo(const char *nome_arquivo, const char *info){
     FILE *file = fopen(nome_arquivo, "r");
     char buffer[256];
+    long valor;
     
     if(!file){
         perror("Erro ao abrir o arquivo");
@@ -15,18 +16,33 @@ int ler_arquivo(const char *nome_arquivo, const char *info){
     while(fscanf(file, "%s", buffer) != EOF){
 
         if(strcmp(buffer, info) == 0){
-            fscanf(file, "%s", buffer);
-            printf("%s %s\n", info, buffer);
+            fscanf(file, "%ld", &valor);
             fclose(file);
-            return 0;
+            return valor; // configurar para que o char vire int
         }
     }
     fclose(file);
-    return 1;
+    return -1;
 }
 
 int main(){
-    ler_arquivo("/proc/meminfo", "MemTotal:");
+    long mem_total = ler_arquivo("/proc/meminfo", "MemTotal:");
+    if(mem_total == -1){
+        fprintf(stderr, "Erro ao ler a memória total\n");
+        return 1;
+    }
+    long mem_free = ler_arquivo("/proc/meminfo", "MemAvailable:");
+    if(mem_free == -1){
+        fprintf(stderr, "Erro ao ler a memória disponível\n");
+        return 1;
+    }
+
+    long mem_used = mem_total - mem_free;
+
+    printf("Memória Total: %ld GB\n", mem_total/(1024*1024)); // transformar em float
+    printf("Memória Disponível: %ld GB\n", mem_free/(1024*1024)); // transformar em float
+    printf("Memória Usada: %ld GB\n", mem_used/(1024*1024)); // transformar em float
+
 
 
     return 0;
